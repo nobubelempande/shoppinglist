c247ca28e69e3541ad41d3ab52de62b59c7bd301
Developer Delisa <delisamav3@gmail.com>
2022-05-21 10:54:03 +0200

    Edited Tabbed Inventory Activity - Removed Unused Imports

--- app/src/main/java/com/viiishoppinglistapp/doit/TabbedInventoryActivity.java
+++ app/src/main/java/com/viiishoppinglistapp/doit/TabbedInventoryActivity.java
@@ -5,34 +5,19 @@ import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
 
-import com.viiishoppinglistapp.doit.Adapters.InventoryItemsAdapter;
-import com.viiishoppinglistapp.doit.Model.modelItem;
-import com.viiishoppinglistapp.doit.Model.modelShoppingList;
-import com.viiishoppinglistapp.doit.Utils.DatabaseHandler;
 import com.viiishoppinglistapp.doit.databinding.ActivityTabbedInventoryBinding;
 import com.viiishoppinglistapp.doit.ui.inventory.InventorySectionsPagerAdapter;
 
-import java.util.List;
 
 public class TabbedInventoryActivity extends AppCompatActivity {
 
     private ActivityTabbedInventoryBinding binding;
-
-    private RecyclerView rvInventory;
-    private InventoryItemsAdapter adapter;
-
-    private DatabaseHandler db;
-
-    private List<modelItem> allInventoryItems;
-    private modelShoppingList currShoppingList;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
