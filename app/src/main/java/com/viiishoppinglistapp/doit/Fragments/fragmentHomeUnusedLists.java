5e6e2ed70d5d97e88ee112b9855ba5f5f7bb528f
Developer Delisa <delisamav3@gmail.com>
2022-05-21 08:42:27 +0200

    Edited Home Shopping Lists Fragments

--- app/src/main/java/com/viiishoppinglistapp/doit/Fragments/fragmentHomeUnusedLists.java
+++ app/src/main/java/com/viiishoppinglistapp/doit/Fragments/fragmentHomeUnusedLists.java
@@ -1,6 +1,58 @@
 package com.viiishoppinglistapp.doit.Fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.viiishoppinglistapp.doit.Model.modelItem;
+import com.viiishoppinglistapp.doit.Model.modelShoppingList;
+import com.viiishoppinglistapp.doit.R;
+import com.viiishoppinglistapp.doit.TabbedHomeActivity;
+import com.viiishoppinglistapp.doit.Utils.DatabaseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class fragmentHomeUnusedLists extends Fragment {
+
+    TabbedHomeActivity activity;
+    UnusedShoppingListsAdapter adapter;
+
+    RecyclerView rvUnusedShoppingLists;
+
+    DatabaseHandler db;
+
+    List<modelItem> allShoppingLists;
+    modelShoppingList currShoppingList;
+
+    final Context mContext;
+
+
+    public fragmentHomeUnusedLists(Context C, TabbedHomeActivity activity){
+        this.mContext = C;
+        this.activity = activity;
+        allShoppingLists = new ArrayList<>();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //setupInventory();
+
+        return inflater.inflate(R.layout.home_unused_shopping_lists_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupInventory(view);
+    }
 }
--- app/src/main/java/com/viiishoppinglistapp/doit/Fragments/fragmentHomeUsedLists.java
+++ app/src/main/java/com/viiishoppinglistapp/doit/Fragments/fragmentHomeUsedLists.java
@@ -1,6 +1,58 @@
 package com.viiishoppinglistapp.doit.Fragments;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.viiishoppinglistapp.doit.Model.modelItem;
+import com.viiishoppinglistapp.doit.Model.modelShoppingList;
+import com.viiishoppinglistapp.doit.R;
+import com.viiishoppinglistapp.doit.TabbedHomeActivity;
+import com.viiishoppinglistapp.doit.Utils.DatabaseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class fragmentHomeUsedLists extends Fragment {
+
+    TabbedHomeActivity activity;
+    UsedShoppingListsAdapter adapter;
+
+    RecyclerView rvUsedShoppingLists;
+
+    DatabaseHandler db;
+
+    List<modelItem> allShoppingLists;
+    modelShoppingList currShoppingList;
+
+    final Context mContext;
+
+
+    public fragmentHomeUsedLists(Context C, TabbedHomeActivity activity){
+        this.mContext = C;
+        this.activity = activity;
+        allShoppingLists = new ArrayList<>();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //setupInventory();
+
+        return inflater.inflate(R.layout.home_used_shopping_lists_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupInventory(view);
+    }
 }
