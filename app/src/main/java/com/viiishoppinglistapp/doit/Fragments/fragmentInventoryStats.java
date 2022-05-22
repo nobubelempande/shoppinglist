e7e87917edcb8dddc21fe425244cba5245821997
Developer Delisa <delisamav3@gmail.com>
2022-05-22 16:22:56 +0200

    Added PieChart to Fragment - Setup and Load PieChart Data Functions on Fragment - Fixed page refresh - Edited Dialog Button

--- app/src/main/java/com/viiishoppinglistapp/doit/Fragments/fragmentInventoryStats.java
+++ app/src/main/java/com/viiishoppinglistapp/doit/Fragments/fragmentInventoryStats.java
@@ -1,6 +1,7 @@
 package com.viiishoppinglistapp.doit.Fragments;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.viiishoppinglistapp.doit.Adapters.InventoryItemsAdapter;
 import com.viiishoppinglistapp.doit.Model.modelItem;
 import com.viiishoppinglistapp.doit.Model.modelShoppingList;
@@ -26,7 +35,7 @@ public class fragmentInventoryStats extends Fragment {
     TabbedInventoryActivity activity;
     InventoryItemsAdapter adapter;
 
-    RecyclerView rvInventory;
+    PieChart pieChart;
 
     DatabaseHandler db;
 
@@ -47,4 +56,49 @@ public class fragmentInventoryStats extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_layout_inventory_stats, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        pieChart = getActivity().findViewById(R.id.pieChart_inventoryStats);
+
+        loadPieChartData();
+        setupPieChart();
+    }
+
+    private void loadPieChartData() {
+        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
+        entries.add(new PieEntry(0.4f, "First Element"));
+        entries.add(new PieEntry(0.25f, "Second Element"));
+        entries.add(new PieEntry(0.25f, "Third Element"));
+        entries.add(new PieEntry(0.1f, "Forth Element"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "Elements");
+        dataSet.setColors(ColorTemplate.LIBERTY_COLORS);
+        dataSet.setDrawValues(true);
+        dataSet.setValueTextSize(12);
+        dataSet.setValueTextColor(Color.DKGRAY);
+        dataSet.setValueFormatter(new PercentFormatter(pieChart));
+
+        PieData data = new PieData(dataSet);
+        pieChart.setData(data);
+        pieChart.invalidate();
+    }
+
+    private void setupPieChart() {
+        pieChart.setUsePercentValues(true);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setDescription(new Description());
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setHoleRadius(75f);
+        pieChart.setTransparentCircleRadius(33f);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend L = pieChart.getLegend();
+        L.setEnabled(false);
+
+        pieChart.animateY(1200);
+    }
+
 }
