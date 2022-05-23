5f01f8aaa3219c84d0202fb4ffa2fb6960ed8240
TakundaDanha <takunda.danha@icloud.com>
2022-05-23 12:27:25 +0000

    Testing
    Signed-off-by: K-Mohau <2332113@students.wits.ac.za>
    Signed-off-by: TakundaDanha <takunda.danha@icloud.com>

---
+++ app/src/test/java/com/viiishoppinglistapp/doit/Interface/ExpiryDateInterfaceTest.java
@@ -0,0 +1,18 @@
+package com.viiishoppinglistapp.doit.Interface;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExpiryDateInterfaceTest {
+    ExpiryDateInterface ageCalculator = new ExpiryDateInterface();
+
+    @Test
+    public void calculateAgeTest() {
+        String age = ageCalculator.calculateAge("31-03-2000");
+
+        assertEquals(age, "21");
+
+    }
+
+}
