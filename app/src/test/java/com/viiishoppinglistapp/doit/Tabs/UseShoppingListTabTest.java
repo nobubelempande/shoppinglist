67d2f5e0a8a9c4c146fce78eb3d539962cb18d6d
NobubeleMpande <nobumpande28@gmail.com>
2022-05-23 13:04:26 +0000

    testing

---
+++ app/src/test/java/com/viiishoppinglistapp/doit/Tabs/UseShoppingListTabTest.java
@@ -0,0 +1,19 @@
+package com.viiishoppinglistapp.doit.Tabs;
+
+
+        import static org.junit.Assert.*;
+
+        import org.junit.Test;
+
+public class UseShoppingListTabTest {
+    UseShoppingListTab ageCalculator = new UseShoppingListTab();
+
+    @Test
+    public void calculateAgeTest() {
+        String age = ageCalculator.calculateAge("31-03-2000");
+
+        assertEquals(age, "21");
+
+    }
+
+}
