eea4a6b61de667075d568b85be8fa564b348dc3b
NobubeleMpande <nobumpande28@gmail.com>
2022-05-23 14:17:26 +0000

    test

---
+++ app/src/test/java/com/viiishoppinglistapp/doit/ShopActivities/DeleteShopTest.java
@@ -0,0 +1,28 @@
+package com.viiishoppinglistapp.doit.ShopActivities;
+
+import static org.junit.Assert.*;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DeleteShopTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+    DeleteShop ageCalculator = new DeleteShop();
+
+    @Test
+    public void calculateAgeTest() {
+        String age = ageCalculator.calculateAge("31-03-2000");
+
+        assertEquals(age, "21");
+
+    }
+    //
+
+
+}
---
+++ app/src/test/java/com/viiishoppinglistapp/doit/ShopActivities/ShopListTest.java
@@ -0,0 +1,29 @@
+package com.viiishoppinglistapp.doit.ShopActivities;
+
+import static org.junit.Assert.*;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ShopListTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    ShopList ageCalculator = new ShopList();
+
+    @Test
+    public void calculateAgeTest() {
+        String age = ageCalculator.calculateAge("31-03-2000");
+
+        assertEquals(age, "21");
+
+    }
+    //
+
+
+}
