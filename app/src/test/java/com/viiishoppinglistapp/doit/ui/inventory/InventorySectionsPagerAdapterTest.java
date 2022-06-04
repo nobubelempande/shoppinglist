f66f42b6634d56e60a7b3bfe631e5662a1dd9449
Padisho <2322257@students.wits.ac.za>
2022-06-04 15:55:33 +0200

    InventorySectionsPagerAdapterTest

---
+++ app/src/test/java/com/viiishoppinglistapp/doit/ui/inventory/InventorySectionsPagerAdapterTest.java
@@ -0,0 +1,48 @@
+package com.viiishoppinglistapp.doit.ui.inventory;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import androidx.fragment.app.FragmentManager;
+
+import com.viiishoppinglistapp.doit.TabbedHomeActivity;
+import com.viiishoppinglistapp.doit.TabbedInventoryActivity;
+import com.viiishoppinglistapp.doit.ui.home.HomeSectionsPagerAdapter;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class InventorySectionsPagerAdapterTest extends TestCase {
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+    }
+
+    @Test
+    public void getItem() {
+    }
+
+    @Test
+    public void getPageTitle() {
+    }
+
+    @Test
+    public void getCount() {
+        Context context=new TabbedHomeActivity();
+        TabbedInventoryActivity s=new TabbedInventoryActivity();
+        FragmentManager p= s.getSupportFragmentManager();
+        InventorySectionsPagerAdapter m=new InventorySectionsPagerAdapter(context,p,s);
+        m.getCount();
+        assertEquals(2,m.getCount());
+    }
+
+    @Test
+    public void getItemPosition() {
+    }
+}
